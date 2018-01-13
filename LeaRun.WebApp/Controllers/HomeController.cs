91bc7ea03a60beaae3ab3276cefe4736a3014da1
Jun Shen <991011509@qq.com>
2018-01-13 14:35:57 +0800

    121

--- LeaRun.WebApp/Controllers/HomeController.cs
+++ LeaRun.WebApp/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ using System.Web.Mvc;
 
 namespace LeaRun.WebApp.Controllers
 {
+    
     public class HomeController : Controller
     {
         public Base_ModuleBll base_modulebll = new Base_ModuleBll();
